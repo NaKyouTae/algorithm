2cae8f08f1fe3fd4bb3cf03dbcf107c68fcea4c3
나규태 <qppk@naver.com>
2021-05-15 01:57:42 +0900

    Moo 게임 문제 스터디

--- src/algorithm/codility/test.java
+++ src/algorithm/codility/test.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.checkerframework.checker.units.qual.Current;
 import org.junit.Test;
 
 public class test {
--- src/algorithm/dividenconquer/Moo.java
+++ src/algorithm/dividenconquer/Moo.java
@@ -3,32 +3,39 @@ package algorithm.dividenconquer;
 import java.util.Scanner;
 
 public class Moo {
-    public static void main(String[] args) {
+    public static String ans;
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        if(n == 1) {
-            System.out.println("m");
-        }else if(n <= 3) {
-            System.out.println("o");
-        }else {
-            System.out.println(moo(n));
-        }
+
+        moo(n);
+
+        System.out.println(ans);
     }
 
-    public static String moo(int num) {
+    public static void moo(int num) {
         int side = 3;
-        int center = 4;
+        int center = 0;
 
-        while(num > side) {
-            side = center + side * 2;
-            center++;
-        }
-        System.out.println(side);
-        if(num == side+1) {
-            return "m";
+        if(num <= 3){
+            ans = (num == 1) ? "m" : "o";
         }else {
-            return "o";
+            while(num > side) {
+                side = center + 4 + side * 2;
+                center++;
+            }
+            System.out.println("side : " + side);
+            System.out.println("center : " + center);
+            int fb = (side - center - 3) / 2;
+            System.out.println("fb : " + fb);
+            if(side-fb+1 <= num) {
+                moo(num-side+fb);
+            }else if(num == fb+1){
+                ans = "m";
+            }else {
+                ans = "o";
+            }
         }
     }
 }
