31fb04208a33e64dbe9c815ffe972197181114ee
나규태 <qppk@naver.com>
2021-06-07 23:46:36 +0900

    강의실 완료, 순회강연 진행 중

--- src/algorithm/study5/iectureRoom.java
+++ src/algorithm/study5/iectureRoom.java
@@ -3,9 +3,7 @@ package algorithm.study5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class iectureRoom {
     public static class Node {
@@ -16,12 +14,14 @@ public class iectureRoom {
             this.e = e;
         }
     }
-    public static PriorityQueue<Node> que = new PriorityQueue<>(new Comparator<>() {
+    public static List<Node> list = new ArrayList<>();
+    public static PriorityQueue<Integer> que = new PriorityQueue<>();
+    public static Comparator<Node> comparator = new Comparator<>() {
         @Override
         public int compare(Node o1, Node o2) {
-            return Integer.compare(o1.o, o2.o);
+            return Integer.compare(o1.s, o2.s);
         }
-    });
+    };
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -33,23 +33,20 @@ public class iectureRoom {
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            que.add(new Node(o, s, e));
+            list.add(new Node(o, s, e));
         }
+
+        Collections.sort(list, comparator);
 
         int max = Integer.MIN_VALUE;
 
-        while(!que.isEmpty()) {
-            Node node = que.poll();
-            int cnt = 0;
-            while(!que.isEmpty()) {
-                Node childNode = que.peek();
-
-                if(node.e < childNode.s) continue;
-
+        for(int i = 0; i < n; i++) {
+            while(!que.isEmpty() && que.peek() <= list.get(i).s) {
                 que.poll();
-                cnt++;
             }
-            max = Math.max(max, cnt);
+
+            que.add(list.get(i).e);
+            max = Math.max(max, que.size());
         }
 
         System.out.println(max);
