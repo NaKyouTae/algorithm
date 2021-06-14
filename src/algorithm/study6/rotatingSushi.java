14f4609da718202b53bbe7dba88907c13d7a3c25
나규태 <qppk@naver.com>
2021-06-15 00:58:00 +0900

    2021-06-14(월) 회전 초밥 Queue 복습 중

--- src/algorithm/study6/rotatingSushi.java
+++ src/algorithm/study6/rotatingSushi.java
@@ -3,10 +3,7 @@ package algorithm.study6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -16,7 +13,7 @@ public class rotatingSushi {
 
     public static int nu, ma, di, cp;
     public static int max = Integer.MIN_VALUE;
-    public static List<Integer> sushis = new ArrayList<>();
+    public static Queue<Integer> sushis = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -27,51 +24,59 @@ public class rotatingSushi {
         di = Integer.parseInt(commend[2]);
         cp = Integer.parseInt(commend[3]);
 
+        Map<Integer, Integer> eat = new HashMap<>();
 
-        sushis = new ArrayList<>();
+        eat.put(cp, 1);
 
         for(int i = 0; i < nu; i++) {
             int sushi = Integer.parseInt(br.readLine());
             sushis.add(sushi);
-        }
-
-        int s = (nu / 2) - 1 + di;
-        int e = (nu - s);
-
-        for(int i = 0; i < sushis.size() - (di - 1); i++) {
-            List<Integer> one = new ArrayList<>();
-            List<Integer> two = new ArrayList<>();
-
-            int oc = 0, tc = 0, o, t;
-            if(i < s) one = sushis.subList(i, i+di);
-            if(i >= e) two = sushis.subList(i, i+di);
-
-            List<Integer> oDisc = new ArrayList<>();
-            List<Integer> tDisc = new ArrayList<>();
-
-            for(int j = 0; j < di; j++) {
-                if(one.size() > 0 && one.size() <= di) {
-                    if(one.get(j) == cp) oc++;
-                    if(!oDisc.contains(one.get(j))) oDisc.add(one.get(j));
-                }
-
-                if(two.size() > 0 && two.size() <= di) {
-                    if(two.get(j) == cp) tc++;
-                    if(!tDisc.contains(two.get(j))) tDisc.add(two.get(j));
+            if(i < di) {
+                if(eat.containsKey(sushi)) {
+                    eat.put(sushi, eat.get(sushi)+1);
+                }else {
+                    eat.put(sushi, 1);
                 }
             }
+        }
 
-            int os = (oc == 0) ? oDisc.size()+1 : oDisc.size();
-            int ts = (tc == 0) ? tDisc.size()+1 : tDisc.size();
-            int m = Math.max(os, ts);
+        for(int i = di; i < sushis.size() + di; i++) {
+            System.out.println(eat.toString());
+            counting(eat.size());
 
-            dishsCounting(m);
+            int e = getHashMap(eat, 0);
+
+            if(eat.containsKey(e) && eat.get(e) >= 2) {
+                eat.put(e, eat.get(e)-1);
+            }else {
+                eat.remove(e);
+            }
+
+            int sushi = getQueue(sushis, (i >= sushis.size()) ? sushis.size() - i : i);
+
+            if(!eat.containsKey(sushi)) {
+                eat.put(sushi, 1);
+            }else {
+                eat.put(sushi, eat.get(sushi)+1);
+            }
         }
 
         System.out.println(max);
     }
 
-    public static void dishsCounting(int num) {
+    public static void counting(int num) {
         max = Math.max(num, max);
+    }
+    public static int getQueue(Queue<Integer> que, int idx) {
+        Iterator<Integer> iter = que.iterator();
+        int res = 0;
+        for(int i = 0; i <= idx; i++) res = iter.next();
+        return res;
+    }
+    public static int getHashMap(Map<Integer, Integer> map, int idx) {
+        Iterator<Integer> iter = map.keySet().iterator();
+        int res = 0;
+        for(int i = 0; i <= idx; i++) res = iter.next();
+        return res;
     }
 }
